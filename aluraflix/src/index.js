36d39c32b08e7ca2b011e58dd2ac8c3425ff70c6
Mario Tonelli <mario.tonelli@driven.cx>
2020-07-28 02:59:28 -0300

    Dando inicio no react router dom

--- aluraflix/src/index.js
+++ aluraflix/src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo(){
+  return(
+    <div>
+      Página de Cadastro de Video
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={App} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
