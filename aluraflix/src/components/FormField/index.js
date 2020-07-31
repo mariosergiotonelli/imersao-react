bbb41b9fe375aa211eb382e26153ac6cae80ce98
Mario Tonelli <mario.tonelli@driven.cx>
2020-07-31 02:03:53 -0300

    Atualizando arquivos com heroku

--- aluraflix/src/components/FormField/index.js
+++ aluraflix/src/components/FormField/index.js
@@ -1,19 +1,108 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-import React from "react";
+const FormFieldWrapper = styled.div`
+  position: relative;
 
-function FormField({ label, type, name, value, onChange }) {
+  textarea {
+    min-height: 150px;
+  }
+
+  input[type="color"] {
+    padding-left: 56px;
+  }
+`;
+
+const Label = styled.label``;
+
+Label.Text = styled.span`
+  align-items: center;
+  color: #E5E5E5;
+  display: flex;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  height: 57px;
+  left: 16px;
+  position: absolute;
+  top: 0;
+  transform-origin: 0% 0%;
+  transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585D;
+  border-bottom: 4px solid #53585D;
+  border-radius: 4px;
+  border-top: 4px solid transparent;
+  border: 0;
+  color: #F5F5F5;
+  display: block;
+  font-size: 18px;
+  height: 57px;
+  margin-bottom: 45px;
+  outline: 0;
+  padding: 16px 16px;
+  resize: none;
+  transition: border-color .3s;
+  width: 100%;
+
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+
+  &:focus:not([type='color']) + ${Label.Text} {
+    transform: scale(.6) translateY(-10px);
+  }
+
+  ${({ value }) => {
+    const hasValue = value.length > 0;
+    return hasValue && css`
+        &:not([type='color']) + ${Label.Text} {
+          transform: scale(.6) translateY(-10px);
+        }
+      `;
+  }
+}
+`;
+
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const isTypeTextArea = type === 'textarea';
+  const tag = isTypeTextArea ? 'textarea' : 'input';
+
   return (
-    <div>
-      <label>
-        {label}
-        <input
+    <FormFieldWrapper>
+      <Label>
+        <Input
+          as={tag}
           type={type}
           value={value}
           name={name}
           onChange={onChange}
         />
-      </label>
-    </div>
-  )
+        <Label.Text>
+          {label}
+          :
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
 }
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
 export default FormField;
--- aluraflix/src/pages/cadastro/Categoria/index.js
+++ aluraflix/src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -9,75 +9,114 @@ function CadastroCategoria() {
     descricao: '',
     cor: '',
   }
-
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
-  function setValue(chave, valor){
+
+  function setValue(chave, valor) {
+    // chave: nome, descricao, bla, bli
     setValues({
       ...values,
-      [chave]: valor,
+      [chave]: valor, // nome: 'valor'
     })
   }
 
-  function handleChange(infosDoEvento){
-    const { getAttribute, value} = infosDoEvento.target;
+  function handleChange(infosDoEvento) {
     setValue(
       infosDoEvento.target.getAttribute('name'),
       infosDoEvento.target.value
     );
   }
 
-  function handleSubmit(e){
-    e.preventDefault();
-    setCategorias([
-      ...categorias,
-      values.nome
-    ]);
+  // ============
 
-    setValues(valoresIniciais)
-  }
+  useEffect(() => {
+    if(window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+       .then(async (respostaDoServer) =>{
+        if(respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategorias(resposta);
+          return;
+        }
+        throw new Error('Não foi possível pegar os dados');
+       })
+    }
+  }, []);
 
   return (
     <PageDefault>
       <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+
+          setCategorias([
+            ...categorias,
+            values
+          ]);
+
+          setValues(valoresIniciais)
+      }}>
 
         <FormField
-          label="Nome da Categoria "
-          type='text'
-          name='nome'
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
           value={values.nome}
           onChange={handleChange}
         />
 
         <FormField
-          label="Descrição "
-          type='text'
-          name='nome'
+          label="Descrição:"
+          type="????"
+          name="descricao"
           value={values.descricao}
           onChange={handleChange}
         />
+        {/* <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              name="descricao"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
         <FormField
-          label="Cores "
-          type='color'
-          name='Cor'
+          label="Cor"
+          type="color"
+          name="cor"
           value={values.cor}
           onChange={handleChange}
         />
+        {/* <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor}
+              name="cor"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
         <button>
           Cadastrar
         </button>
       </form>
 
+
       <ul>
         {categorias.map((categoria, indice) => {
           return (
             <li key={`${categoria}${indice}`}>
-              {categoria}
+              {categoria.titulo}
             </li>
           )
         })}
